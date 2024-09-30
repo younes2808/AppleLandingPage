b2b9d89990d6c9c58c0c53e253006ead1c1e52b0
younes2808 <benhaida.mehdi@gmail.com>
2024-09-30 12:43:33 +0200

    fjerner filer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
-import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
